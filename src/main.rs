4640bb36b69aaa7c1d25f0c30144ad877bdded9e
cncgl <concigel@gmail.com>
2016-05-05 13:17:51 +0900

    add update

--- src/main.rs
+++ src/main.rs
@@ -78,7 +78,17 @@ fn main() {
   });
 
   // update
+  router.put("/api/todos/:id", middleware! { |_req, mut _res|
+    let todo  = _req.json_as::<Todo>().unwrap();
+    let status = todo.status;
+    let title  = todo.title.to_string();
+    println!("update status {:?} title {}", status, title);
 
+    let json_obj = Json::from_str("{}").unwrap();
+    _res.set(MediaType::Json);
+    _res.set(StatusCode::Ok);
+    return _res.send(json_obj);
+  });
 
   server.utilize(router);
   server.listen("127.0.0.1:6767");
